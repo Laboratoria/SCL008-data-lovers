8a57ed140ced73410519d06badb152050bc51ef6
nerdale <alexandra.neira@gmail.com>
2019-02-17 21:47:14 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+/* Manejo de data */
+
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
+
+const example = () => {
+  return 'example';
+};
+
+window.example = example;
---
+++ test/data.spec.js
@@ -0,0 +1,16 @@
+global.window = global;
+global.assert = require('chai').assert;
+require('../src/data');
+require('./data.spec.js');
+
+
+describe('example', () => {
+  
+  it('debería ser una función', () => {
+    assert.equal(typeof example, 'function');
+  });
+
+  it('debería retornar "example"', () => {
+    assert.equal(example(), 'example');
+  });
+})
