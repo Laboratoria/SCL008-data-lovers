74686633dc243047fd46ac3b7e0d4e72451e3390
KarenQuezadaMejia <mac@MacBook-Pro-de-mac.local>
2019-02-27 14:50:38 -0300

    se modifica

--- src/data.js
+++ src/data.js
@@ -1,62 +1,77 @@
-window.data = {
+/* Manejo de data */
 
-    filterData: (data, condition) => {
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
 
-        if (condition === "none") {
-            filtrada = POKEMON.pokemon;
-        } else {
+window.data ={
+    
+    filterData: (data,condition) => {
+
+        if (condition==="none") 
+        {
+            filtrada=POKEMON.pokemon;
+        } else 
+        {
             filtrada = POKEMON.pokemon.filter(element => {
-                if (element.type.includes(condition)) {
+                if(element.type.includes(condition))
+                {
                     return element;
                 }
             });
         }
-
-        return filtrada;
-
+    
+    return filtrada;
+    
     },
 
-    sortData: (data, sortBy) => {
+    sortData: (data,sortBy) => {
 
-        if (sortBy === "a-z") {
-            ordenada = data.sort((a, b) => {
-                if (a.name < b.name)
-                    return -1;
-                else if (a.name > b.name)
+        if(sortBy ==="a-z") 
+        {
+            ordenada = data.sort( (a, b) => {
+                    if( a.name < b.name ) 
+                        return -1;
+                    else if( a.name > b.name )
+                        return 1;
+                    else
+                        return 0;
+                }); 
+        }
+        else if (sortBy ==="z-a") 
+        {
+            ordenada = data.sort( (a, b) => {
+                if( a.name < b.name ) 
                     return 1;
-                else
-                    return 0;
-            });
-        } else if (sortBy === "z-a") {
-            ordenada = data.sort((a, b) => {
-                if (a.name < b.name)
-                    return 1;
-                else if (a.name > b.name)
+                else if( a.name > b.name )
                     return -1;
                 else
                     return 0;
             });
 
-        } else if (sortBy === '1-150' || sortBy === 'none') {
-            ordenada = data.sort((a, b) => {
-                if (a.id < b.id)
+        }
+        else if (sortBy==='1-150'||sortBy==='none')
+        {
+            ordenada= data.sort( (a, b) => {
+                if( a.id < b.id ) 
                     return -1;
-                else if (a.id > b.id)
+                else if( a.id > b.id )
                     return 1;
                 else
                     return 0;
             });
-        } else if (sortBy === '150-1') {
-            ordenada = data.sort((a, b) => {
-                if (a.id < b.id)
+        }
+        else if (sortBy==='150-1')
+        {
+            ordenada= data.sort( (a, b) => {
+                if( a.id < b.id ) 
                     return 1;
-                else if (a.id > b.id)
+                else if( a.id > b.id )
                     return -1;
                 else
                     return 0;
             });
         }
-
+            
         return ordenada;
 
 
